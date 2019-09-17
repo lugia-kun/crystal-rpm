15e5baabea74c2ddde0d7bc78f4bd7f7128d9cc3
Hajime Yoshimori <lugia.kun@gmail.com>
2019-09-18 07:59:12 +0900

    Improve docs.

--- src/rpm/tagdata.cr
+++ src/rpm/tagdata.cr
@@ -1,42 +1,58 @@
 module RPM
+  # Stores one RPM TagData.
   class TagData
+    # Handy module to define methods in `ReturnType` classes.
     module ReturnTypeModule
+      # :nodoc:
+      #
+      # `struct`s cannot be inherited without `abstract`.
+      # So we provides initialize method for each `struct`.
       def initialize(@ptr)
       end
 
+      # :nodoc:
+      #
+      # Copy constructor.
       def initialize(other : ReturnTypeBase)
         @ptr = other.@ptr
       end
 
+      # Returns the number of elements in the tagdata.
+      #
+      # This method is not type-dependent, but `Indexable` module
+      # requires that `#size` method must be defined **after** the
+      # inclusion of the module.
       def size
         raise NilAssertionError.new if @ptr.null?
         LibRPM.rpmtdCount(@ptr)
       end
     end
 
+    # Base class of `ReturnType` classes.
     abstract struct ReturnTypeBase
       @ptr : LibRPM::TagData = Pointer(Void).null.as(LibRPM::TagData)
 
-      def detach
-        self.class.new(LibRPM.rpmtdFree(@ptr))
-      end
-
+      # Returns the tag value.
       def tag
         Tag.from_value(LibRPM.rpmtdTag(@ptr))
       end
 
+      # Returns the type of TagData
       def type
         LibRPM.rpmtdType(@ptr)
       end
 
+      # Deallocates the tagdata.
+      #
+      # This method modifies `self`.
       def detach
         @ptr = LibRPM.rpmtdFree(@ptr)
       end
 
       # Sets tag value.
       #
-      # NOTE: RPM allows to change tag value only to same type.
-      #       If not, this method raises `TypeCastError`.
+      # NOTE: RPM allows to change tag value only to same type. If
+      # not, this method raises `TypeCastError`.
       def tag=(tag : Tag | TagValue)
         if LibRPM.rpmtdSetTag(@ptr, tag) == 0
           raise TypeCastError.new("Incompatible tag value #{tag} for #{self.class}")
@@ -44,11 +60,13 @@ module RPM
         tag
       end
 
+      # Set current index of TagData.
       private def pos=(idx)
         raise NilAssertionError.new if @ptr.null?
         LibRPM.rpmtdSetIndex(@ptr, idx)
       end
 
+      # Format the TagData at current index in given format.
       def format1(fmt : TagDataFormat)
         s = LibRPM.rpmtdFormat(@ptr, fmt, nil)
         if s.null?
@@ -61,6 +79,8 @@ module RPM
         end
       end
 
+      # Format the TagData to a string also represents array and empty
+      # TagData, and send to given IO.
       def format(io : IO, fmt : TagDataFormat)
         count = self.size
         if count < 1
@@ -85,9 +105,14 @@ module RPM
       end
     end
 
+    # Generic ReturnType class for integral types.
     abstract struct ReturnType(T) < ReturnTypeBase
+      # We needs a method to get pointer data.
       abstract def fetch_ptr : Pointer(T)
 
+      # Returns binary raw array of expecting type.
+      #
+      # Raises `NilAssertionError` if `#fetch_ptr` returns `NULL`.
       def bytes : Slice(T)
         self.pos = 0
         ptr = fetch_ptr
@@ -98,7 +123,11 @@ module RPM
         end
       end
 
-      def to_a : Array(T)
+      # Returns the array of values using `#bytes`
+      #
+      # This may be faster than `Indexable#to_a` (via
+      # `Enumerable#to_a`).
+      def to_a_from_bytes : Array(T)
         b = bytes
         Array(T).build(b.size) do |buffer|
           b.copy_to(buffer, b.size)
@@ -106,98 +135,147 @@ module RPM
         end
       end
 
+      # :nodoc:
       def unsafe_fetch(idx)
         self.pos = idx
         fetch_ptr.value
       end
     end
 
+    # UInt8 type for TagData
+    #
+    # RPM does not support obtaining the pointer of UInt8 array.
+    # (`rpmtdGetChar` returns pointer only for `CHAR` typed tags.
+    #  Note that this property won't change even if you use
+    #  `TagData#force_return_type!`)
     struct ReturnTypeInt8 < ReturnTypeBase
       include Indexable(UInt8)
       include ReturnTypeModule
 
+      # :nodoc:
       def unsafe_fetch(idx)
         self.pos = idx
         LibRPM.rpmtdGetNumber(@ptr).to_u8
       end
 
+      # :nodoc:
       def bytes
         raise TypeCastError.new("Cannot take byte array of UInt8.")
       end
 
+      # :nodoc:
       def fetch_ptr
         raise TypeCastError.new("Cannot fetch pointer of UInt8.")
       end
     end
 
+    # UInt16 type for TagData
     struct ReturnTypeInt16 < ReturnType(UInt16)
       include Indexable(UInt16)
       include ReturnTypeModule
 
+      # :nodoc:
       def fetch_ptr
         LibRPM.rpmtdGetUint16(@ptr)
       end
 
+      # :nodoc:
       def bytes
         super
       end
+
+      # :nodoc:
+      def to_a
+        to_a_from_bytes
+      end
     end
 
+    # UInt32 type for TagData
     struct ReturnTypeInt32 < ReturnType(UInt32)
       include Indexable(UInt32)
       include ReturnTypeModule
 
+      # :nodoc:
       def fetch_ptr
         LibRPM.rpmtdGetUint32(@ptr)
       end
 
+      # :nodoc:
       def bytes
         super
       end
+
+      # :nodoc:
+      def to_a
+        to_a_from_bytes
+      end
     end
 
+    # UInt64 type for TagData
     struct ReturnTypeInt64 < ReturnType(UInt64)
       include Indexable(UInt64)
       include ReturnTypeModule
 
+      # :nodoc:
       def fetch_ptr
         LibRPM.rpmtdGetUint64(@ptr)
       end
 
+      # :nodoc:
       def bytes
         super
       end
+
+      # :nodoc:
+      def to_a
+        to_a_from_bytes
+      end
     end
 
+    # String and array of String type for TagData
     struct ReturnTypeString < ReturnTypeBase
       include Indexable(String)
       include ReturnTypeModule
 
+      # :nodoc:
       def unsafe_fetch(idx)
         self.pos = idx
-        String.new(LibRPM.rpmtdGetString(@ptr))
+        str = LibRPM.rpmtdGetString(@ptr)
+        if str.null?
+          raise NilAssertionError.new("rpmtdGetString returned NULL (type mismatch?)")
+        else
+          String.new(str)
+        end
       end
 
+      # :nodoc:
       def bytes
         raise TypeCastError.new("Cannot take byte array of string(s).")
       end
 
+      # :nodoc:
       def fetch_ptr
         raise TypeCastError.new("Cannot fetch pointer of string type.")
       end
-
-      # Use `Indexable(String)#to_a` for `#to_a`.
     end
 
+    # Char type for TagData
+    #
+    # NOTE: Some RPM tags use this type for enum types (integral
+    # type). Because this class converts values to `Char`, If you want
+    # integral type, you may want to read them like `UInt8` using
+    # `TagData#force_return_type!`
     struct ReturnTypeChar < ReturnTypeBase
       include Indexable(Char)
       include ReturnTypeModule
 
+      # :nodoc:
       def unsafe_fetch(idx)
         self.pos = idx
         LibRPM.rpmtdGetChar(@ptr).value.chr
       end
 
+      # :nodoc:
       def bytes : Slice(UInt8)
         self.pos = 0
         ptr = fetch_ptr
@@ -208,19 +286,26 @@ module RPM
         end
       end
 
+      # :nodoc:
       def fetch_ptr
         LibRPM.rpmtdGetChar(@ptr)
       end
     end
 
+    # Binary type for TagData
+    #
+    # NOTE: The raw representation of binary data a same to array of
+    # UInt8, but same reason to UInt8, we cannot obtain it directly.
     struct ReturnTypeBin < ReturnTypeBase
       include Indexable(Bytes)
       include ReturnTypeModule
 
+      # :nodoc:
       def unsafe_fetch(idx)
         bytes
       end
 
+      # Converts hexadecimal character to integral value.
       private def hex2bin(hex)
         {% begin %}
           case hex
@@ -238,6 +323,7 @@ module RPM
         {% end %}
       end
 
+      # Generates binary data from hexadecimal string representation.
       def bytes
         f = format1(TagDataFormat::STRING)
         fsz = f.size // 2
@@ -249,6 +335,7 @@ module RPM
         end
       end
 
+      # :nodoc:
       def fetch_ptr
         raise TypeCastError.new("Cannot fetch pointer of binary type.")
       end
@@ -256,6 +343,8 @@ module RPM
 
     @ptr : ReturnTypeBase
 
+    # Sets `ReturnType` class to work with associated tag, and new
+    # wrapped `TagData` class, for given pointer.
     private def self.for(ptr : LibRPM::TagData)
       type = LibRPM.rpmtdType(ptr)
       r = case type
@@ -279,9 +368,15 @@ module RPM
       new(r)
     end
 
+    # :nodoc:
+    #
+    # Use `.create` methods.
     def initialize(@ptr)
     end
 
+    # Creates a new TagData pointer.
+    #
+    # Raises `RPM::AllocationError` if upstream API returns `NULL`.
     private def self.new_ptr
       ptr = LibRPM.rpmtdNew
       if ptr.null?
@@ -290,6 +385,14 @@ module RPM
       ptr
     end
 
+    # Create a new tagdata with initializing it with given block.
+    #
+    # This method is primitive method. Passed argument is raw value of
+    # `rpmtd` (actually a pointer).
+    #
+    # If given block returns `0`, it will be treated as failure to set
+    # tagdata, and raises `TypeCastError`. You can raise another class
+    # of `Exception` in the block if you want to do so.
     def self.create(&block)
       ptr = new_ptr
       begin
@@ -303,6 +406,14 @@ module RPM
       for(ptr)
     end
 
+    # Create a new tagdata with initializing it with given block.
+    #
+    # This method is primitive method. Passed argument is raw value of
+    # `rpmtd` (actually a pointer).
+    #
+    # If given block returns `0`, it will be treated as failure to set
+    # tagdata, and returns `nil`. You can **NOT** raise an exception in
+    # the block, or make sure to rescue them.
     def self.create?(&block)
       ptr = new_ptr
       if yield(ptr) == 0
@@ -313,64 +424,73 @@ module RPM
       end
     end
 
+    # Creates a new tagdata which stores given string.
     def self.create(str : String, tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromString(ptr, tag, str)
       end
     end
 
+    # Creates a new tagdata which stores given array of strings.
     def self.create(stra : Array(String), tag : Tag | TagValue)
-      data = Array(Pointer(UInt8)).new(stra.size)
-      stra.each do |str|
-        data << str.to_unsafe
+      data = Array(Pointer(UInt8)).new(stra.size) do |i|
+        stra[i].to_unsafe
       end
       create do |ptr|
         LibRPM.rpmtdFromStringArray(ptr, tag, data, stra.size)
       end
     end
 
+    # Creates a new tagdata which stores given UInt8 value.
     def self.create(u8 : UInt8, tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromUint8(ptr, tag, pointerof(u8), 1)
       end
     end
 
+    # Creates a new tagdata which stores given UInt16 value.
     def self.create(u16 : UInt16, tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromUint16(ptr, tag, pointerof(u16), 1)
       end
     end
 
+    # Creates a new tagdata which stores given UInt32 value.
     def self.create(u32 : UInt32, tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromUint32(ptr, tag, pointerof(u32), 1)
       end
     end
 
+    # Creates a new tagdata which stores given UInt64 value.
     def self.create(u64 : UInt64, tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromUint64(ptr, tag, pointerof(u64), 1)
       end
     end
 
+    # Creates a new tagdata which stores given array of UInt8 values.
     def self.create(u8a : Array(UInt8) | Bytes, tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromUint8(ptr, tag, u8a, u8a.size)
       end
     end
 
+    # Creates a new tagdata which stores given array of UInt16 values.
     def self.create(u16a : Array(UInt16) | Slice(UInt16), tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromUint16(ptr, tag, u16a, u16a.size)
       end
     end
 
+    # Creates a new tagdata which stores given array of UInt32 values.
     def self.create(u32a : Array(UInt32) | Slice(UInt32), tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromUint32(ptr, tag, u32a, u32a.size)
       end
     end
 
+    # Creates a new tagdata which stores given array of UInt64 values.
     def self.create(u64a : Array(UInt64) | Slice(UInt64), tag : Tag | TagValue)
       create do |ptr|
         LibRPM.rpmtdFromUint64(ptr, tag, u64a, u64a.size)
@@ -384,8 +504,8 @@ module RPM
 
     # Sets tag value
     #
-    # NOTE: RPM allows to change tag value only to same type.
-    #       If not, this method raises `TypeCastError`.
+    # NOTE: RPM allows to change tag value only to same type. If not,
+    # this method raises `TypeCastError`.
     def tag=(val)
       @ptr.tag = val
     end
@@ -414,7 +534,7 @@ module RPM
     # Returns true if tag data is array
     #
     # Returns true if number of elements is greater than 1, the return
-    # type is `TagReturnType::ARRAY` or the type is
+    # type is `TagReturnType::ARRAY`, or the type is
     # `TagType::STRING_ARRAY`.
     def is_array?
       type = self.type
@@ -523,6 +643,14 @@ module RPM
       format(TagDataFormat::BASE64)
     end
 
+    # Returns raw binary data of TagData
+    #
+    # Some types do not suitable for getting binary data. If so, this
+    # method raises `TypeCastError`.
+    def bytes
+      @ptr.bytes
+    end
+
     def to_s(io)
       format(io, TagDataFormat::STRING)
     end
@@ -554,7 +682,8 @@ module RPM
     # Returns old ReturnType class.
     #
     # CHAR (`ReturnTypeChar`), INT8 (`ReturnTypeUInt8`) are
-    # interoperable. Otherwise, it may cause unexpected behavior.
+    # interoperable (except for `#bytes`). Otherwise, it may cause
+    # unexpected behavior.
     def force_return_type!(type : ReturnTypeBase.class)
       cls = @ptr.class
       @ptr = type.new(@ptr)
