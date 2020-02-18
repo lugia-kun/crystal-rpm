925d2d735cb9f3770ba48ad0664b447225050049
Hajime Yoshimori <lugia.kun@gmail.com>
2020-02-19 00:27:08 +0900

    [travis] Use compiler provided on the Travis

--- .travis-build.sh
+++ .travis-build.sh
@@ -8,13 +8,21 @@ die() {
 set -x
 
 export CRYSTAL_VERSION=$(crystal eval "puts Crystal::VERSION")
-if [[ "${CRYSTAL_VERSION/-dev/}" != "${CRYSTAL_VERSION}" ]]; then
-  die "Nightly build not supported"
-fi
-export CRYSTAL_RELEASE=${CRYSTAL_VERSION#*-}
-export CRYSTAL_VERSION=${CRYSTAL_VERSION%%-*}
+export CRYSTAL_COMMIT=$(crystal eval "puts Crystal::BUILD_COMMIT")
 export CRYSTAL_DIST=o
 export DOCKERFILE=.travis/Dockerfile.$RPMVERSION
+
+workdir=$(pwd)
+if [[ $CRYSTAL_DIST == l ]]; then # legacy
+    curl -LO https://github.com/lugia-kun/crystal-for-legacy-dist/releases/download/0.32.1-1/crystal-${CRYSTAL_VERSION}-1.sles11.x86_64.tar.xz || die "Legacy dist does not support nightly build"
+    xz -dc crystal-${CRYSTAL_VERSION}-1.sles11.x86_64.tar.xz >$workdir/crystal.tar
+else # official
+    pushd /snap/crystal/current
+    tar -cf $workdir/crystal.tar .
+    popd
+fi
+trap "rm -f $workdir/crystal.tar" 0 1 2
+
 
 id=$(git log -n1 --format=%H -- $DOCKERFILE)
 tag=$RPMVERSION-crystal-$CRYSTAL_DIST-$CRYSTAL_VERSION-$CRYSTAL_RELEASE
@@ -27,7 +35,7 @@ fi
 if [[ "$image_id" ==  "$id" ]]; then
     docker tag lugiakun/crystal-rpm:$tag $RPMVERSION
 else
-    docker build -t $RPMVERSION --target=$RPMVERSION-$CRYSTAL_DIST --build-arg CRYSTAL_VERSION=${CRYSTAL_VERSION} --build-arg CRYSTAL_RELEASE=${CRYSTAL_RELEASE} --build-arg COMMIT_ID=$id -f ${DOCKERFILE} . || die "Failed to build ${tag}"
+    docker build -t $RPMVERSION --target=$RPMVERSION --build-arg CRYSTAL_VERSION=${CRYSTAL_VERSION} --build-arg CRYSTAL_RELEASE=${CRYSTAL_RELEASE} --build-arg COMMIT_ID=$id -f ${DOCKERFILE} . || die "Failed to build ${tag}"
     if echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin; then
         if [[ -z "$image_id" ]] || [[ $(git log "$image_id..$id" | wc -l) -gt 0 ]]; then
             docker tag $RPMVERSION lugiakun/crystal-rpm:$tag
